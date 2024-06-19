10cbbe05a3aa07fc1d28f3e171fd3f85dd98cf94
luquinhas10 <162441126+luquinhas10@users.noreply.github.com>
2024-06-19 08:38:15 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,33 +1,34 @@
 class Programa:
     def __init__(self, nome, ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome(self,novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
         
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
     
 
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self._likes} Likes')
 
     
-
 class Filme(Programa):
    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
    
 
@@ -35,20 +36,23 @@ class Filme(Programa):
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome,ano)
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
 
    
 vingadores = Filme('Vingadores - Guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.ano} 'f'{vingadores.duracao} - {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}'
-f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
+
+filmes_e_series = [vingadores,atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
+    
